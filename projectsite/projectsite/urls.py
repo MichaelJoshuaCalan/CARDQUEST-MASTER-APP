ccd75a7ef611cd5a7c457437a224458d86139671
MichaelJoshuaCalan <mr.swabe.xx12345@gmail.com>
2023-12-03 14:03:42 +0000

    DUNNO

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from cardquest.views import HomePageView
+from cardquest import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path ('',views.HomePageView.as_view(), name='home'),
 ]
